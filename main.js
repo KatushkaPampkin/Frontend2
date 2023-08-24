83b2f999024e1102a571f1db05d5be5109340eb8
KatushkaPampkin <131025811+KatushkaPampkin@users.noreply.github.com>
2023-08-24 20:54:54 +0300

    Task-9

---
+++ main.js
@@ -0,0 +1,33 @@
+//числа
+let num1 =23;
+console.log(num1);
+// строки
+let string1= "Don't worry,";
+let string2= "be happy";
+console.log(string1, string2);
+// неопределенная переменная
+let variable1;
+console.log(variable1);
+variable1= 25;
+console.log(variable1);
+//variable1= 'smile';
+//console.log(variable1);
+// объект
+let movie ={
+    genre:"Comedy",
+    country:"Russia",
+}
+console.log(movie);
+console.log(movie.genre);
+// булевая
+let bool1 = true
+let bool2 = false
+
+console.log(15<4);
+console.log(15>4);
+
+// арифм. операции
+console.log(15-4);
+console.log(15+4);
+console.log(16/4);
+console.log(15*4);
